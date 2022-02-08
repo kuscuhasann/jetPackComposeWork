b33d44a025faff616da78096d5bcc8e09fdceda8
kuscuhasann <kuscuhasann97@gmail.com>
2022-02-08 11:41:02 +0300

    added 1 box

--- app/src/main/java/com/pakt_games/jetpackcomposework/MainActivity.kt
+++ app/src/main/java/com/pakt_games/jetpackcomposework/MainActivity.kt
@@ -3,13 +3,19 @@ package com.pakt_games.jetpackcomposework
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.pakt_games.jetpackcomposework.ui.theme.JetPackComposeWorkTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Sayfa("Android")
+                    Sayfa()
                 }
             }
         }
@@ -30,14 +36,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Sayfa(name: String) {
-    Text(text = "Hello $name!")
+fun Sayfa() {
+    Row {
+       Box(modifier = Modifier.size(86.dp).background(Color.Red))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetPackComposeWorkTheme {
-        Sayfa("HASAN")
+        Sayfa()
     }
 }
